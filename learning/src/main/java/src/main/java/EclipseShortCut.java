90a128ff457461a3172095c89e91b12005afd2da
MarsYoung <bigmazhiyu@126.com>
2014-05-30 11:01:40 +0800

    创建一个maven学习项目，导入以前的一批工程

---
+++ learning/src/main/java/src/main/java/EclipseShortCut.java
@@ -0,0 +1,19 @@
+package src.main.java;
+
+/**
+ * Eclipse快捷键使用
+ * @author Mars
+ *
+ */
+public class EclipseShortCut {
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+	}
+	
+}
---
+++ learning/src/main/java/src/main/java/MaxNumber.java
@@ -0,0 +1,10 @@
+package src.main.java;
+
+public class MaxNumber {
+
+	public static void main(String[] args) {
+		System.out.println("最大的Integer:"+Integer.MAX_VALUE);
+		System.out.println("最大的Float:"+Float.MAX_VALUE);
+		System.out.println("最大的Double:"+Double.MAX_VALUE);
+	}
+}
